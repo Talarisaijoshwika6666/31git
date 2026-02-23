2608f0606f69abb68c4f186abee4c38c98628826
Talarisaijoshwika6666 <saijoshwikatalari@gmail.com>
2026-02-23 21:26:14 +0530

    Fix print statement for subtraction

--- sum.py
+++ sum.py
@@ -2,6 +2,6 @@ num1 = 5
 num2 = 4
 sum = num1+num2
 print("The sum of two numbers:",sum)
-print("The difference of two numbers:",difference)
+print("The subtraction of two numbers:",subtraction)
 print("The multiplication of two numbers:",multiplication)
 print("The division of two numbers:",division)
