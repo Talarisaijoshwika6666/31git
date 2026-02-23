87aa5cfc3790bbefebbb74057cbd6a099d0bf877
Talarisaijoshwika6666 <saijoshwikatalari@gmail.com>
2026-02-23 21:24:21 +0530

    Add operations for difference, multiplication, and division

--- sum.py
+++ sum.py
@@ -2,3 +2,6 @@ num1 = 5
 num2 = 4
 sum = num1+num2
 print("The sum of two numbers:",sum)
+print("The difference of two numbers:",difference)
+print("The multiplication of two numbers:",multiplication)
+print("The division of two numbers:",division)
